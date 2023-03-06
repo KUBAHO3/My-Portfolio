96c99bd2030ff4363d0e1b257165c3e9cbcbc209
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-03-06 17:05:25 +0530

    add mobile nav

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("The js file is loaded successfully")
+const menu = ReadElement('#menu-icon');
+const nav = ReadElement('.mobile-nav');
+const close_nav = ReadElement('.close-button');
+const choose = ReadElement('.mobile-nav');
+
+AddMenu(menu);
+RemoveMenu(close_nav);
+RemoveMenu(choose);
+
+function AddMenu (selected){
+    return selected.addEventListener('click', () => nav.classList.add('nav-toggle'));
+}
+
+function RemoveMenu (selected){
+    return selected.addEventListener('click', () => nav.classList.remove('nav-toggle'));
+}
+
+function ReadElement (selector){
+    return document.querySelector(selector);
+}
