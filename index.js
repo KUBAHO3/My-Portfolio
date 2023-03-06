904a0432f557772d0b4f50b65f7a056f14351ecd
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-03-06 19:40:04 +0530

    add eslint & fixed the erros

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
+function ReadElement(selector) {
+  return document.querySelector(selector);
+}
+
 const menu = ReadElement('#menu-icon');
 const nav = ReadElement('.mobile-nav');
-const close_nav = ReadElement('.close-button');
+const closeNav = ReadElement('.close-button');
 const choose = ReadElement('.mobile-nav');
 
+function AddMenu(selected) {
+  return selected.addEventListener('click', () => nav.classList.add('nav-toggle'));
+}
+
+function RemoveMenu(selected) {
+  return selected.addEventListener('click', () => nav.classList.remove('nav-toggle'));
+}
+
 AddMenu(menu);
-RemoveMenu(close_nav);
+RemoveMenu(closeNav);
 RemoveMenu(choose);
-
-function AddMenu (selected){
-    return selected.addEventListener('click', () => nav.classList.add('nav-toggle'));
-}
-
-function RemoveMenu (selected){
-    return selected.addEventListener('click', () => nav.classList.remove('nav-toggle'));
-}
-
-function ReadElement (selector){
-    return document.querySelector(selector);
-}
