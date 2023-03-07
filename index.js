1227acd23c2bcd272d1781285b6a8cd3f7c8129c
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-03-07 09:33:45 +0530

    fix linter errors

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const menu = ReadElement('#menu-icon');
 const nav = ReadElement('.mobile-nav');
 const WorkModal = ReadElement('.modal-section');
 const closeNav = ReadElement('.close-button');
-let viewModal = ReadElement('.view-modal');
+const viewModal = ReadElement('.view-modal');
 
 function AddMenu(selected, modalPart, opener) {
   return selected.addEventListener('click', () => modalPart.classList.add(opener));
@@ -22,19 +22,22 @@ RemoveMenu(WorkModal, WorkModal, 'nav-toggle');
 RemoveMenu(closeNav, nav, 'nav-toggle');
 RemoveMenu(nav, nav, 'nav-toggle');
 
-const details = {
-  data: [
-    {
-      "title":"Tonic",
-      "name":"CANOPY",
-      "technology": "Back End Dev",
-      "year": "2025",
-      "imageUrl":"images/Snapshoot-Portfolio.png",
-      "description":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-      "languages": ['html', 'css', 'javascript', 'github', 'ruby', 'Bootstraps'],
-      "liveLink": "https://www.linkedin.com/in/kubaho-linne-heaven-78ab37208/",
-      "sourceCode": "https://github.com/KUBAHO3",
-    },
-  ],
-};
-
+// const details = {
+//   data: [
+//     {
+//       title: 'Tonic',
+//       name: 'CANOPY',
+//       technology: 'Back End Dev',
+//       year: '2025',
+//       imageUrl: 'images/Snapshoot-Portfolio.png',
+//       description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+//                     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+//                     when an unknown printer took a galley of type and scrambled it to
+//                     make a type specimen book. It has survived not only five centuries,
+//                     but also the leap into electronic typesetting, remaining essent",
+//       languages: ['html', 'css', 'javascript', 'github', 'ruby', 'Bootstraps'],
+//       liveLink: 'https://www.linkedin.com/in/kubaho-linne-heaven-78ab37208/',
+//       sourceCode: 'https://github.com/KUBAHO3',
+//     },
+//   ],
+// };
